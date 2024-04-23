a22058899d2953e64f9f01bfc071b590885c9505
Thilo Fromm <thilofromm@microsoft.com>
2024-04-23 14:46:03 +0200

    config.toml: add link to rss feed to sidebar menu
    Also, improve local-server to take care of git submodules.
    
    Signed-off-by: t-lo <thilo.alexander@gmail.com>

--- local-server.sh
+++ local-server.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
+# Only required after initial checkout. Here to be on the safe side.
+git submodule init
+git submodule update
+
+# Render pages and start hugo server. The server will auto-update content on changes.
 hugo server --disableFastRender
